24bc5c32ce8d81f8f663c6de37089d9ffdd28fc6
robert-nganga <robertnganga220@gmail.com>
2023-06-08 10:29:00 +0300

    profile screen top app bar

--- app/src/main/java/com/nganga/robert/chargelink/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/nganga/robert/chargelink/ui/screens/ProfileScreen.kt
@@ -2,8 +2,10 @@ package com.nganga.robert.chargelink.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -12,10 +14,38 @@ import androidx.compose.ui.Modifier
 fun ProfileScreen(){
     Scaffold(
         topBar = {
+            TopAppBar(
+                title = {
+                    Text("Profile")
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {  }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(
+                        onClick = {  }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.MoreVert,
+                            contentDescription = "more options"
+                        )
+                    }
+                }
+
+            )
         }
+
     ) { contentPadding->
         Column(modifier = Modifier.padding(contentPadding)) {
 
         }
     }
 }
+
