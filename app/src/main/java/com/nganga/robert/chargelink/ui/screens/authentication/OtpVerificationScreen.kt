94794669bf3ff5863322078d0fbca911bb52c9fb
robert-nganga <robertnganga220@gmail.com>
2023-06-16 20:18:33 +0300

    added otp verification card

--- app/src/main/java/com/nganga/robert/chargelink/ui/screens/authentication/OtpVerificationScreen.kt
+++ app/src/main/java/com/nganga/robert/chargelink/ui/screens/authentication/OtpVerificationScreen.kt
@@ -1,6 +1,28 @@
 package com.nganga.robert.chargelink.ui.screens.authentication
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
-fun OtpVerificationScreen(){}
+fun OtpVerificationScreen(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
+
+@Composable
+fun OtpVerificationCard(
+    modifier: Modifier = Modifier,
+    otp: String,
+    onOtpChange: (String)->Unit
+){
+
+}
