c4a1d1fb6ce82170d8650f8720754f4b5948c8b0
robert-nganga <robertnganga220@gmail.com>
2023-06-16 20:39:50 +0300

    added top app bar in otp verification screen

--- app/src/main/java/com/nganga/robert/chargelink/ui/screens/authentication/OtpVerificationScreen.kt
+++ app/src/main/java/com/nganga/robert/chargelink/ui/screens/authentication/OtpVerificationScreen.kt
@@ -3,9 +3,15 @@ package com.nganga.robert.chargelink.ui.screens.authentication
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun OtpVerificationScreen(){
@@ -24,5 +30,21 @@ fun OtpVerificationCard(
     otp: String,
     onOtpChange: (String)->Unit
 ){
+
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(20.dp),
+        border = null,
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background
+        )
+    ){
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+
+        }
+    }
 
 }
