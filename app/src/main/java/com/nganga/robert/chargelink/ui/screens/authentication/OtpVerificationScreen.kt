3a4bb3b7a17dc20676a5210736c980091cee9976
robert-nganga <robertnganga220@gmail.com>
2023-06-17 09:37:58 +0300

    added otp text field

--- app/src/main/java/com/nganga/robert/chargelink/ui/screens/authentication/OtpVerificationScreen.kt
+++ app/src/main/java/com/nganga/robert/chargelink/ui/screens/authentication/OtpVerificationScreen.kt
@@ -1,25 +1,70 @@
 package com.nganga.robert.chargelink.ui.screens.authentication
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.nganga.robert.chargelink.R
 
 @Composable
 fun OtpVerificationScreen(){
+
+    var otpValue by remember{
+        mutableStateOf("")
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(
+                modifier = Modifier.padding(vertical = 10.dp),
+                onClick = {  }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Back"
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(40.dp))
+        Text(
+            text = stringResource(id = R.string.otp_verification),
+            style = MaterialTheme.typography.titleLarge
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Icon(
+            painter = painterResource(id = R.drawable.ic_push_notifications),
+            contentDescription = "OTP Icon",
+            tint = MaterialTheme.colorScheme.primary
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        OtpVerificationCard(
+            otp = otpValue,
+            onOtpChange = { otpValue = it }
+        )
 
     }
 }
@@ -41,9 +86,63 @@ fun OtpVerificationCard(
         )
     ){
         Column(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp, vertical = 10.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
+            Text(
+                text = stringResource(id = R.string.otp_verification),
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            BasicTextField(
+                value = otp,
+                onValueChange = {
+                    if (it.length <= 5){
+                        onOtpChange(it)
+                    }
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                ),
+                decorationBox = {
+                    Row(horizontalArrangement = Arrangement.Center) {
+                        repeat(times = 5){ index ->
+                            val char = when {
+                                index >= otp.length -> ""
+                                else -> otp[index].toString()
+                            }
+                            val isFocused = otp.length == index
+                            Text(
+                                text = char,
+                                modifier = Modifier
+                                    .width(40.dp)
+                                    .border(
+                                        width = if (isFocused) 2.dp else 1.dp,
+                                        color = if (isFocused) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.outline,
+                                        shape = RoundedCornerShape(8.dp)
+                                    ),
+                                style = MaterialTheme.typography.titleMedium,
+                                textAlign = TextAlign.Center,
+                                color = MaterialTheme.colorScheme.onBackground
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                        }
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(
+                onClick = {  },
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier.align(Alignment.End)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.continues)
+                )
+            }
         }
     }
 
