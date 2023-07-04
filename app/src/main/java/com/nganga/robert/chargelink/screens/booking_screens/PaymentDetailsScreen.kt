22362d2ae2d4a5d9cbeda1cc352c37837a4a4761
robert-nganga <robertnganga220@gmail.com>
2023-07-04 12:07:11 +0300

    Extracted BookingBottomBar as component

--- app/src/main/java/com/nganga/robert/chargelink/screens/booking_screens/PaymentDetailsScreen.kt
+++ app/src/main/java/com/nganga/robert/chargelink/screens/booking_screens/PaymentDetailsScreen.kt
@@ -7,28 +7,30 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
 import androidx.compose.material.icons.outlined.CreditCard
 import androidx.compose.material.icons.outlined.Payments
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalTextInputService
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import com.nganga.robert.chargelink.R
 
 
@@ -81,6 +83,107 @@ fun PaymentDetailsScreen() {
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardDetailsForm(
+
+){
+
+    var cardNumber by rememberSaveable{
+        mutableStateOf("")
+    }
+    var expiryDate by rememberSaveable{
+        mutableStateOf("")
+    }
+    var cvv by rememberSaveable{
+        mutableStateOf("")
+    }
+    var country by rememberSaveable{
+        mutableStateOf("Kenya")
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(
+            text = stringResource(id = R.string.card_information),
+            style = MaterialTheme.typography.titleMedium,
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        OutlinedTextField(
+            value = cardNumber,
+            onValueChange = {
+                cardNumber = it
+            },
+            placeholder = {
+                Text(
+                    text = stringResource(id = R.string.card_number),
+                    style = MaterialTheme.typography.labelMedium
+                )
+            },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Outlined.CreditCard,
+                    contentDescription = "Card Number"
+                )
+            },
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                value = expiryDate,
+                onValueChange = {
+                    expiryDate = it
+                },
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.expiry_date),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                },
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            OutlinedTextField(
+                value = cvv,
+                onValueChange = {
+                    cvv = it
+                },
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.cvc),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                },
+                trailingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_cvc),
+                        contentDescription = "Card Number"
+                    )
+                }
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = stringResource(id = R.string.country_or_region),
+            style = MaterialTheme.typography.titleMedium,
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        CountrySelector(
+            onCountrySelectionChange = {
+                country = it
+            },
+            country = country
+        )
+    }
+}
+
+
 
 @Composable
 fun PaymentMethodTabView(
@@ -183,6 +286,72 @@ fun PaymentDetailsTopBar(
             maxLines = 1,
             overflow = TextOverflow.Ellipsis
         )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CountrySelector(
+    modifier: Modifier = Modifier,
+    country: String = "",
+    onCountrySelectionChange: (String)->Unit
+){
+    var expanded by remember { mutableStateOf(false) }
+    val countries = listOf("Kenya", "India", "USA", "UK", "Canada", "Australia", "Japan", "China", "Russia", "France", "Germany", "Italy", "Spain", "Brazil", "Mexico", "Argentina", "Chile", "Peru", "Colombia", "South Africa", "Egypt", "Nigeria", "Ethiopia", "Morocco", "Tanzania", "Algeria", "Sudan", "Uganda", "Ghana", "Mozambique", "Angola", "Somalia", "Ivory Coast", "Madagascar", "Cameroon", "Niger", "Burkina Faso", "Mali", "Malawi", "Zambia", "Senegal", "Chad", "Zimbabwe", "Guinea", "Rwanda", "Benin", "Burundi", "Tunisia", "South Sudan", "Togo", "Sierra Leone", "Libya", "Congo", "Liberia", "Central African Republic", "Mauritania", "Eritrea", "Namibia", "Gambia", "Botswana", "Gabon", "Lesotho", "Guinea-Bissau", "Equatorial Guinea", "Mauritius", "Eswatini", "Djibouti", "Comoros", "Cabo Verde", "Sao Tome & Principe", "Seychelles")
+
+
+    var textFieldSize by remember { mutableStateOf(Size.Zero)}
+
+    val icon = if (expanded)
+        Icons.Filled.ArrowDropUp //it requires androidx.compose.material:material-icons-extended
+    else
+        Icons.Filled.ArrowDropDown
+
+
+    Box(modifier = modifier) {
+        CompositionLocalProvider(
+            LocalTextInputService provides null
+        ) {
+            OutlinedTextField(
+                value = country,
+                onValueChange = { onCountrySelectionChange(it) },
+                modifier = Modifier
+                    .fillMaxWidth(0.6f)
+                    .onFocusChanged {
+                        if (it.isFocused) {
+                            expanded = !expanded
+                        }
+                    }
+                    .onGloballyPositioned { coordinates ->
+                        //This value is used to assign to the DropDown the same width
+                        textFieldSize = coordinates.size.toSize()
+                    },
+                label = { Text(stringResource(id = R.string.gender)) },
+                trailingIcon = {
+                    Icon(icon,"contentDescription",
+                        Modifier.clickable { expanded = !expanded })
+                }
+            )
+        }
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current){textFieldSize.width.toDp()})
+        ) {
+            countries.forEach { label ->
+                DropdownMenuItem(
+                    onClick = {
+                        onCountrySelectionChange(label)
+                        expanded = false
+                    },
+                    text = {
+                        Text(text = label)
+                    }
+                )
+            }
+        }
     }
 }
 
---
+++ app/src/main/java/com/nganga/robert/chargelink/ui/components/BookingBottomBar.kt
@@ -0,0 +1,62 @@
+package com.nganga.robert.chargelink.ui.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.nganga.robert.chargelink.R
+
+
+@Composable
+fun BooKingBottomBar(
+    onBackButtonClicked: () -> Unit,
+    onNextButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
+            .padding(horizontal = 20.dp, vertical = 10.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        OutlinedButton(
+            onClick = {
+                onBackButtonClicked.invoke()
+            },
+            shape = RoundedCornerShape(10.dp),
+            border = BorderStroke(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.secondary
+            )
+        ) {
+            Text(
+                text = stringResource(id = R.string.back)
+            )
+        }
+
+        Button(
+            onClick = {
+                onNextButtonClicked.invoke()
+            },
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.secondary
+            )
+        ) {
+            Text(
+                text = stringResource(id = R.string.continues)
+            )
+        }
+    }
+}
